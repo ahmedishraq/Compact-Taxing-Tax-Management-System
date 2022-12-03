e321fd20d36f7225645917adcc6d2f71031f8e53
ahmedishraq <ishraqahmedbd@gmail.com>
2022-12-03 19:07:54 +0600

    register & user model added

--- Code/Compact Taxing - Tax Management System/src/main/java/Model/RegisterModel.java
+++ Code/Compact Taxing - Tax Management System/src/main/java/Model/RegisterModel.java
@@ -9,5 +9,31 @@ package Model;
  * @author ahmed_ishraq <https://github.com/ahmedishraq>
  */
 public class RegisterModel {
+    private String email;
+    private String id;
+    private String pass;
     
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getId(){
+        return id;
+    }
+    
+    public void setPass(String pass){
+        this.pass = pass;
+    }
+    
+    public String getPass(){
+        return pass;
+    }
 }
